fce7846e0fbce9f200a77890b1ae73b53abfac56
Alex Cameron <102175979+acameron724@users.noreply.github.com>
2022-06-22 15:49:17 -0400

    Update palindromeNumber.js

--- palindromeNumber.js
+++ palindromeNumber.js
@@ -5,9 +5,9 @@
 
 var isPalindrome = function(x) {
     let palindrome = false
-    let forwardsNum = x.toString().split("")
-    let lengthOfArray = forwardsNum.length
-    let backwardsNum = []
+    const forwardsNum = x.toString().split("")
+    const lengthOfArray = forwardsNum.length
+    const backwardsNum = []
     
     for (let i = lengthOfArray-1; i>= 0; i--) {
         backwardsNum.push(forwardsNum[i])
